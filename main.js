45fba490f2a1694f8ac8c96b2e23beb8cb6bdcb9
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-06-28 19:07:39 +0100

    Add loading screen

--- main.js
+++ main.js
@@ -5,14 +5,24 @@ const {app, BrowserWindow} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow() {
+function createWindow(loading) {
   win = new BrowserWindow({frame: false, show:false})
   win.loadURL(config['remote_url'])
 
   // When ready, show the window
   win.once('ready-to-show', () => {
     win.show()
+    loading.hide()
   })
 }
 
-app.on('ready', createWindow)
+
+app.on('ready', () => {
+  let loading = new BrowserWindow({show: false, frame: false})
+  loading.loadFile(__dirname + '/loading.html')
+  loading.once('ready-to-show', () => {
+    loading.show()
+  })
+
+  createWindow(loading)
+})
